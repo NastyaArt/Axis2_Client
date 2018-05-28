127198bdf8dae6c8316d128f0c2a6effe502ba0c
NastyaArt <artemchuk.nastya@gmail.com>
2018-05-28 15:00:04 +0300

    del waste comments

--- src/client/Client.java
+++ src/client/Client.java
@@ -3,7 +3,6 @@ package client;
 import java.util.Arrays;
 import org.apache.ws.axis2.MySQLServiceEquipmentStub;
 import org.apache.ws.axis2.MySQLServiceEquipmentStub.*;
-import java.rmi.RemoteException;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -11,46 +10,6 @@ public class Client {
 
     private final static String END_POINT = "http://localhost:1997/axis2/services/MySQLServiceEquipment?wsdl";
     private MySQLServiceEquipmentStub stub;
-
-    public static void main(String[] args) throws RemoteException {
-//get my spechial code is working
-/*        MySQLServiceEquipmentStub stub = new MySQLServiceEquipmentStub(END_POINT);
-        MySQLServiceEquipmentStub.GetMySpechialCode codeOperation = new MySQLServiceEquipmentStub.GetMySpechialCode();
-
-        MySQLServiceEquipmentStub.GetMySpechialCodeResponse response = stub.getMySpechialCode(codeOperation);
-
-        int returnedCode = response.get_return();
-        System.out.println("Code: " + returnedCode);
-*/
-
-//        MySQLServiceEquipmentStub stub = new MySQLServiceEquipmentStub(END_POINT);
-//        MySQLServiceEquipmentStub.GetEquipment equipmentOperation = new MySQLServiceEquipmentStub.GetEquipment();
-//        MySQLServiceEquipmentStub.GetEquipmentResponse response = stub.getEquipment(equipmentOperation);
-//        MySQLServiceEquipmentStub.Equipment[] returnedEquipment = response.get_return();
-//        System.out.println("Category: " + returnedEquipment[1].getCategory());
-//
-//        MySQLServiceEquipmentStub.GetCabinet cabinetOperation = new MySQLServiceEquipmentStub.GetCabinet();
-//        MySQLServiceEquipmentStub.GetCabinetResponse response1 = stub.getCabinet(cabinetOperation);
-//        MySQLServiceEquipmentStub.Cabinet[] returnedCabinet = response1.get_return();
-//        System.out.println("CodeResponsible: " + returnedCabinet[1].getCodeResponsible());
-//
-//        MySQLServiceEquipmentStub.GetResponsible responsibleOperation = new MySQLServiceEquipmentStub.GetResponsible();
-//        MySQLServiceEquipmentStub.GetResponsibleResponse response2 = stub.getResponsible(responsibleOperation);
-//        MySQLServiceEquipmentStub.Responsible[] returnedResponsible = response2.get_return();
-//        System.out.println("Code: " + returnedResponsible[1].getCode());
-//
-//        MySQLServiceEquipmentStub.GetDistribution distributionOperation = new MySQLServiceEquipmentStub.GetDistribution();
-//        MySQLServiceEquipmentStub.GetDistributionResponse response3 = stub.getDistribution(distributionOperation);
-//        MySQLServiceEquipmentStub.Distribution[] returnedDistribution = response3.get_return();
-//        System.out.println("Code: " + returnedDistribution[1].getCode());
-//
-//        MySQLServiceEquipmentStub.GetWriteoff writeoffOperation = new MySQLServiceEquipmentStub.GetWriteoff();
-//        MySQLServiceEquipmentStub.GetWriteoffResponse response4 = stub.getWriteoff(writeoffOperation);
-//        MySQLServiceEquipmentStub.Writeoff[] returnedWriteoff = response4.get_return();
-//        System.out.println("Code: " + returnedWriteoff[1].getCode());
-
-
-    }
 
     public void connect(){
         try {
